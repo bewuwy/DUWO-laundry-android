634ed7fb856ece13a6947f1adca9b64478a3b331
bewu <bewuwy@gmail.com>
2024-11-17 11:35:15 +0100

    added button to open multiposs website in browser

--- app/src/main/java/dev/bewu/duwolaundry/MainActivity.java
+++ app/src/main/java/dev/bewu/duwolaundry/MainActivity.java
@@ -1,11 +1,14 @@
 package dev.bewu.duwolaundry;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -61,12 +65,35 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_watcher, R.id.nav_settings)
+                R.id.nav_home, R.id.nav_watcher, R.id.nav_website, R.id.nav_settings)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main_activity);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_website) {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        preferences.getString("multipossURL", "https://duwo.multiposs.nl")
+                ));
+                startActivity(browserIntent);
+                return true;
+            } else {
+                // Fallback for all other (normal) cases.
+                boolean handled = NavigationUI.onNavDestinationSelected(item, navController);
+
+                // This is usually done by the default ItemSelectedListener.
+                // But there can only be one! Unfortunately.
+                if (handled)
+                    drawer.closeDrawer(navigationView);
+
+                // return the result of NavigationUI call
+                return handled;
+            }
+        });
     }
 
     public void updateAvailability(boolean hardRefresh) {
